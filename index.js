be30e20dfc0ca009c0bd199746021a743dedf3cf
Cassava <kidndumbi@gmail.com>
2017-10-05 23:32:57 -0400

    port setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 4119
+app.listen(PORT, () => {
+   
+    console.log("connected to port ${PORT}", PORT )
+
+});
